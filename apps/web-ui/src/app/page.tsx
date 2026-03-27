c356d0b4ce229b7c53e11ff960b63241e74840e7
stanley <stanleygomesdasilva@gmail.com>
2026-03-27 00:03:36 -0300

    chore: removendo codigo

--- apps/web-ui/src/app/page.tsx
+++ apps/web-ui/src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
           onToggleSearch={() => {
             setIsSearchVisible(!isSearchVisible);
             if (isSearchVisible) {
-              setSearchQuery(""); // Clear search when hiding
+              setSearchQuery("");
             }
           }}
           isSearchVisible={isSearchVisible}
