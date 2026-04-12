609bb1a67ad98f4a8477ceda4b0fb0144e3f08ce
stanley <stanleygomesdasilva@gmail.com>
2026-04-12 06:33:58 -0300

    chore: remove paul logo from landpage

--- apps/landpage-ui/src/containers/home/hero-section.tsx
+++ apps/landpage-ui/src/containers/home/hero-section.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
 import { useTranslation } from "react-i18next";
 import { ArrowRight as ArrowRightIcon } from "lucide-react";
 import { motion } from "framer-motion";
@@ -74,14 +73,7 @@ export function HeroSection() {
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.8, delay: 1.2, type: "spring" }}
         className="absolute right-[10%] bottom-[20%] hidden h-40 w-40 overflow-hidden rounded-base border-4 border-border bg-white lg:block shadow-[10px_10px_0px_0px_var(--border)]"
-      >
-        <Image
-          src="/images/logo-paul.jpg"
-          alt="Done Logo"
-          fill
-          className="object-cover"
-        />
-      </motion.div>
+      ></motion.div>
     </section>
   );
 }
